c0b8c3635c8d6626235a637342fbd1e97ad52620
Beyza Keser <beyzaaakeser@gmail.com>
2024-08-15 17:05:31 +0300

    detail  page completed

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './styles/Detail.scss';
 import Container from '../components/Container';
+import Loader from '../components/Loader';
 
 const Detail = () => {
   const params = useParams();
@@ -21,69 +22,63 @@ const Detail = () => {
 
   return (
     <Container>
-      <div className=" flex flex-col gap-6 detail-container">
-        <div className=" flex flex-col lg:flex-row gap-10  ">
-          <div className="flex items-center justify-center">
-            <img
-              src={book?.volumeInfo?.imageLinks?.smallThumbnail}
-              className="detail-img"
-              alt=""
-            />
+      {book ? (
+        <div className=" flex flex-col gap-6 detail-container">
+          <div className=" flex flex-col lg:flex-row gap-10  ">
+            <div className="flex items-center justify-center">
+              <img
+                src={book?.volumeInfo?.imageLinks?.smallThumbnail}
+                className="detail-img"
+                alt=""
+              />
+            </div>
+
+            <table className="table-auto border-collapse hover:border-collapse border border-slate-600">
+              <tbody className="">
+                <BookInfo title={'Name'} value={book?.volumeInfo?.title} />
+                <BookInfo
+                  title={'Author'}
+                  value={book?.volumeInfo?.authors[0]}
+                />
+                <BookInfo
+                  title={'Category'}
+                  value={book?.volumeInfo?.categories[0]}
+                />
+                <BookInfo
+                  title={'Language'}
+                  value={book?.volumeInfo?.language}
+                />
+                <BookInfo title={'Page'} value={book?.volumeInfo?.pageCount} />
+                <BookInfo
+                  title={'Published Date'}
+                  value={book?.volumeInfo?.publishedDate}
+                />
+                <BookInfo
+                  title={'Publisher'}
+                  value={book?.volumeInfo?.publisher}
+                />
+              </tbody>
+            </table>
           </div>
 
-          <table className="table-auto border-collapse hover:border-collapse border border-slate-600">
-            <tbody className=''>
-              <tr>
-                <td className=" border border-slate-300">Name</td>
-                <td className=" border border-slate-300">
-                  {book?.volumeInfo?.title}
-                </td>
-              </tr>
-              <tr>
-                <td className=" border border-slate-300">Author</td>
-                <td className=" border border-slate-300">
-                  {book?.volumeInfo?.authors[0]}
-                </td>
-              </tr>
-              <tr>
-                <td className=" border border-slate-300">Category</td>
-                <td className=" border border-slate-300">
-                  {book?.volumeInfo?.categories[0]}
-                </td>
-              </tr>
-              <tr>
-                <td className=" border border-slate-300">Language</td>
-                <td className="uppercase border border-slate-300">
-                  {book?.volumeInfo?.language}
-                </td>
-              </tr>
-              <tr>
-                <td className=" border border-slate-300">Page</td>
-                <td className=" border border-slate-300">
-                  {book?.volumeInfo?.pageCount}
-                </td>
-              </tr>
-
-              <tr>
-                <td className=" border border-slate-300">Published Date</td>
-                <td className=" border border-slate-300">
-                  {book?.volumeInfo?.publishedDate}
-                </td>
-              </tr>
-              <tr>
-                <td className="border border-slate-300">Publisher</td>
-                <td className="border border-slate-300">{book?.volumeInfo?.publisher}</td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="desc">
+            <p className="">{book?.volumeInfo?.description}</p>
+          </div>
         </div>
-
-        <div className="desc">
-          <p className=''>{book?.volumeInfo?.description}</p>
-        </div>
-      </div>
+      ) : (
+        <Loader />
+      )}
     </Container>
   );
 };
 
 export default Detail;
+
+const BookInfo = ({ title, value }) => {
+  return (
+    <tr>
+      <td className=" border border-slate-300">{title}</td>
+      <td className=" border border-slate-300">{value}</td>
+    </tr>
+  );
+};
